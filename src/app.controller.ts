11dccde67e6908c01754e77541f85693b8b04d91
HernanRPereira <hernxnpereyra@gmail.com>
2024-10-13 21:27:34 -0500

    Get Params Query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,25 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+
+  @Get('products')
+  getProducts(@Query('limit') limit: number = 0, @Query('offset') offset: number = 0, @Query('brand') brand: string) {
+    return `products: limit=> ${limit} offset=> ${offset} brand=> ${brand}`;
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `im product filtro`
+  }
 
   @Get('products/:productId')
   getProduct(@Param('productId') productId: string) {
     return `product ${productId}`
   }
-  
+
   @Get('categories/:categoryId/products/:productId')
   getCategory(@Param('productId') productId: string, @Param('categoryId') categoryId: string) {
     return `product ${productId} and category ${categoryId}`
